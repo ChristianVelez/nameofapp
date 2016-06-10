bebcfed1931485702807be2916b17598a685474e
Christian Velez <christian.velez1@gmail.com>
2016-06-10 18:20:39 -0400

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Product.create(:name => "Public", :description => "Custom made for city riding in style!", :colour => "Black", :price => 299, :image_url => "bikes6.jpg")
 
-Product.create(:name => "Public", :description => "Custom made for city riding in style!", :colour => "Black", :price => 299, :img_url => "bikes.jpg" )
+Product.create(:name => "Schramm", :description => "Custom made to impress all the girls!", :colour => "Black", :price => 279, :image_url => "bikes2.jpg")
 
-Product.create(:name => "Schramm", :description => "Custom made to impress all the girls!", :colour => "Black", :price => 279, :img_url => "bikes2.jpg" )
+Product.create(:name => "Diesel", :description => "Custom made for stealth riding!", :colour => "Black", :price => 279, :image_url => "bikes3.jpg" )
 
-Product.create(:name => "Diesel", :description => "Custom made for stealth riding!", :colour => "Black", :price => 279, :img_url => "bikes3.jpg" )
+Product.create(:name => "Astor", :description => "Custom made for parks and picnics!", :colour => "Orange", :price => 299, :image_url => "bikes4.jpg" )
 
-Product.create(:name => "Astor", :description => "Custom made for parks and picnics!", :colour => "Orange", :price => 299, :img_url => "bikes4.jpg" )
+Product.create(:name => "Abici", :description => "Custom made with love!", :colour => "Baby Blue", :price => 299, :image_url => "bikes.jpg" )
 
-Product.create(:name => "Abici", :description => "Custom made with love!", :colour => "Black", :price => 299, :img_url => "bikes5.jpg" )
+Product.create(:name => "Mayfair", :description => "Custom made for country riding!", :colour => "Red", :price => 299, :image_url => "bikes5.jpg" )
 
-Product.create(:name => "Mayfair", :description => "Custom made for country riding!", :colour => "Black", :price => 299, :img_url => "bikes6.jpg" )
