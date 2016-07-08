53ac28f15669299b8454a5ed0288b11e26cf71bd
Christian Velez <christian.velez1@gmail.com>
2016-07-08 13:35:30 -0400

    Finished styling for email template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "christian.velez1@gmail.com"
+  default from: "contact@bikebk.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,10 @@
 class UserMailer < ApplicationMailer
 	default from: "contact@bikebk.herokuapp.com"
+
+	def contact_form(email, name, message)
+  	@message = message
+    mail(:from => email,
+        :to => 'your-email@example.com',
+        :subject => "A new contact form message from #{name}")
+  end
 end
