ceb690ff047a621852752b1c0bf30e3220443bd0
Christian Velez <christian.velez1@gmail.com>
2016-07-08 10:34:14 -0400

    reformatted contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,12 +9,13 @@ class StaticPagesController < ApplicationController
   end
 
   def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(:from => @email,
-        :to => 'your-email@example.com',
-        :subject => "A new contact form message from #{@name}",
-        :body => @message).deliver_now
-  end
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'your-email@example.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+end
+
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,3 @@
 class UserMailer < ApplicationMailer
-	default from: "from@example.com"
-
-	def contact_form(email, name, message)
-		@message = message
-			mail(:from => email,
-        :to => 'your-email@example.com',
-        :subject => "A new contact form message from #{name}")
-	end
+	default from: "contact@bikebk.herokuapp.com"
 end
