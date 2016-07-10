13ab50ac4a8216043abd847f8147394692fa7fd7
Christian Velez <christian.velez1@gmail.com>
2016-07-10 18:30:00 -0400

    touched up email template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,14 +8,11 @@ class StaticPagesController < ApplicationController
   	@products = Product.limit(3)
   end
 
-  def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(:from => @email,
-      :to => 'christian.velez1@gmail.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,20 +2,21 @@ class UserMailer < ApplicationMailer
   default from: "john@example.com"
 
   def thank_you
-	  @name = params[:name]
-	  @email = params[:email]
-	  @message = params[:message]
-	  UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'christian.velez1@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+end
+
 
 def contact_form(email, name, message)
   @message = message
     mail(:from => email,
         :to => 'your-email@example.com',
-        :subject => "A new contact form message from #{name}") do |format|
-               format.html { render 'contact_form' }
-               format.text { render :text => 'mailer.txt.erb' }
-             end
+        :subject => "A new contact form message from #{name}")
   end
 
 end
