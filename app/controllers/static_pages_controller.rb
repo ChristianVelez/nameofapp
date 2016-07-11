cbd1d4a3591e287e77ab8b67a244017b9b7e4cc3
Christian Velez <christian.velez1@gmail.com>
2016-07-11 08:41:48 -0400

    reset controller actions for email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,9 +10,12 @@ class StaticPagesController < ApplicationController
 
 
   def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'christian.velez1@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,12 @@
 class UserMailer < ApplicationMailer
   default from: "john@example.com"
 
-  def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(:from => @email,
-      :to => 'christian.velez1@gmail.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-end
-
+def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 
 def contact_form(email, name, message)
   @message = message
