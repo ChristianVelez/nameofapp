3f28ec0e9845e1b77fb322d01905ce90c63a09dc
Christian Velez <christian.velez1@gmail.com>
2016-07-11 12:46:45 -0400

    another change to index controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,8 @@ class ProductsController < ApplicationController
     if Rails.env.development? || params[:q]
       search_term = params[:q]
       @products = Product.where("name LIKE ?", "%#{search_term}%")
-    elsif Rails.env.production? || params[:q]
-      search_term = params[:q]
-      @products = Product.where("name iLIKE ?", "%#{search_term}%") 
+    elsif 
+      @products = Product.where("name ilike ?", "%#{search_term}%") 
     else
       @products = Product.all
     end
