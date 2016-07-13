1a6fe382f4d3a9c3d923f594acf9946c1dd7ecf5
Christian Velez <christian.velez1@gmail.com>
2016-07-13 11:31:25 -0400

    changed devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => "login", :sign_out => 'logout'}
+  devise_for :users
 
 
   resources :users
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
-
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
